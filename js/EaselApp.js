e1c2257037b64fb41404a24dfcca309ffa88f5b9
heyimkko <enhasa@gmail.com>
2015-05-17 15:49:38 -0700

    Add react build 0.13.3, easel app component

---
+++ js/EaselApp.js
@@ -0,0 +1,9 @@
+var EaselApp = React.createClass({
+  render: function() {
+    return (
+      <div>My Easel</div>
+    );
+  }
+});
+
+React.render( <EaselApp/>, document.getElementById('easel') );
