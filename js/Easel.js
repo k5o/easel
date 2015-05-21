521060e46907067224a655cd801195af620ad77d
heyimkko <enhasa@gmail.com>
2015-05-21 03:23:28 -0700

    Set up all elements of crude canvas

--- js/Easel.js
+++ js/Easel.js
@@ -28,23 +28,50 @@ var Easel = React.createClass({
     ctx.closePath();
   },
 
-  componentDidMount: function() {
+  componentWillReceiveProps: function(props) {
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
+    var imageObj = new Image();
+    var handle = '@' + props.handle;
 
+    imageObj.src = props.avatar;
+    canvas.width = 1024;
+    canvas.height = 512;
+
+    {/* Outer Rect */}
     context.beginPath();
-    context.rect(0, 0, 1024, 512);
+    context.rect(0, 0, canvas.width, canvas.height);
     context.fillStyle = '#E2EFF5';
     context.fill();
 
+    {/* Inner Rect */}
     this.roundRect(context, 100, 60, 824, 312, 5);
     context.fillStyle = '#617282';
     context.fill();
 
-    {/*Text not showing up because its z-index is lower than the rects for some reason*/}
-    {/*Google font not being used*/}
+    {/* Avatar */}
+    imageObj.onload = function(){
+      context.drawImage(imageObj,130,340,140,140);
+      context.globalCompositeOperation = 'source-over';
+      context.beginPath();
+      context.arc(200, 410, 70, 0, 2*Math.PI, true);
+      context.fill();
+    }
+
+    {/* Handle */}
     context.font = '40px Lato';
-    context.fillText('Hello World!', 130, 340);
+    context.fillStyle = '#313A42';
+    var metrics = context.measureText(handle);
+    var width = metrics.width;
+    context.fillText(handle, 290, 410);
+
+    {/* Name */}
+    context.fillText(props.name, 290 + width + 20, 410);
+
+    {/* Title */}
+    context.font = '36px Lato';
+    context.fillStyle = '#313A42';
+    context.fillText(props.title, 290, 460);
   },
 
   render: function() {
@@ -57,10 +84,6 @@ var Easel = React.createClass({
         <p>
           Edit your name and title directly on the easel
         </p>
-
-        <div style={{textAlign: 'center'}}>
-          <canvas id="myCanvas" width="1024" height="512" style={{margin: 'auto'}}></canvas>
-        </div>
 
         <div className="outer">
           <div className="inner" />
@@ -83,6 +106,10 @@ var Easel = React.createClass({
         <h3>
           Step 3: Save your Easel
         </h3>
+
+        <div style={{textAlign: 'center'}}>
+          <canvas id="myCanvas" width="1024" height="512" style={{margin: 'auto'}}></canvas>
+        </div>
       </div>
     );
   }
